6635a2fd51a0ccf2c002129558ef1cf6c9a89565
Kurren <kurren.nischal@codis.co.uk>
2022-10-31 11:21:04 +0000

    initial

---
+++ Program.cs
@@ -0,0 +1,4 @@
+﻿using System.Management.Automation;
+
+PowerShell.Create().AddCommand("Get-Host").Invoke();
+Console.ReadKey();
---
+++ Util.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PowershellTest
+{
+    public static class Util
+    {
+
+        private static string PrintPSObject(PSObject obj)
+        {
+            var strs = obj.Properties.Select(p => $"{p.Name}: {GetPropertyValue(p)}");
+            return String.Join(Environment.NewLine, strs);
+        }
+        private static string GetPropertyValue(PSPropertyInfo p)
+        {
+            try
+            {
+                return (p.Value.ToString() ?? "");
+            }
+            catch (Exception)
+            {
+                return "";
+            }
+        }
+        public static string PrintPSObjects(IEnumerable<PSObject> objs)
+        {
+            var separator = $"{Environment.NewLine}************{Environment.NewLine}";
+            return String.Join(separator, objs.Select(PrintPSObject));
+        }
+    }
+}
